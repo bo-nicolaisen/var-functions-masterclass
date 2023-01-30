2e82b068197772602fed35a1f9a6f318d64181c6
bo-nicolaisen <31656337+bo-nicolaisen@users.noreply.github.com>
2023-01-30 21:20:44 +0000

    functions updated

--- assets/functions.js
+++ assets/functions.js
@@ -5,3 +5,40 @@ function myNamedFunction() {
 
     console.log('hello from myNamedFunction');
 }
+
+
+// anonymous functions
+
+let anon = function () {
+    console.log('Anonymous function');
+};
+
+anon();
+
+
+// arrow function
+let arrow = () => {
+    console.log('arrow function');
+};
+
+arrow();
+
+
+// function med arguments
+
+function myNamedFunktionArg(argOne, argTwo) {
+    console.log('argument one: ' = argOne = ' argument two: ' = argTwo);
+
+}
+
+
+
+
+// arrow function med arguments
+let arrowArg = (argOne, argTwo) => {
+    console.log('argument one: ' = argOne = ' argument two: ' = argTwo);
+};
+
+arrowArg();
+
+
